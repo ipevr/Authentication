3419540edf8adf36fdef3ba05324033a3a085352
Ingo Peters <ingo.peters2@gmx.de>
2021-01-07 18:54:33 +0100

    Level 4 - Using Hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -20,9 +20,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User = new mongoose.model("User", userSchema);
+
+console.log(md5("123456"));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -39,7 +39,7 @@ app.get("/register", (req, res) => {
 app.post("/login", (req, res) => {
     User.findOne({email: req.body.username}, (err, foundUser) => {
         if (!err) {
-            if (foundUser && foundUser.password === req.body.password) {
+            if (foundUser && foundUser.password === md5(req.body.password)) {
                 console.log("found password: " + foundUser.password);
                 res.render("secrets");
             } else {
@@ -54,7 +54,7 @@ app.post("/login", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save((err) =>{
         if (!err) {res.render("secrets");}
